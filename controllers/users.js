765a26992aecfc9f884d214f2c3a30eb7b3dce03
Gowri239 <gowrimopuru14@gmail.com>
2022-10-11 15:53:33 +0530

    login

--- controllers/users.js
+++ controllers/users.js
@@ -12,7 +12,7 @@ const signup = async (req,res) => {
     try{
         const {name,email,password} = req.body
         if(isStringInvalid(name) || isStringInvalid(email) || isStringInvalid(password)){
-            return res.status(400).json({err: "Enter all details"})
+            return res.status(400).json({message: "Enter all details",success: false})
         }
 
         await User.create({name,email,password})
@@ -20,10 +20,39 @@ const signup = async (req,res) => {
     }catch(err){
         res.status(500).json(err)
     }
-
  }
 
- module.exports = {signup};
+ const login = async(req,res) => {
+    try{
+        const {email,password} = req.body
+        if(isStringInvalid(email) || isStringInvalid(password)){
+            return res.status(400).json({message: "Enter all details",success:false})
+        }
+
+        const user = await User.findAll({where: {email}})
+        if(user.length>0){
+            if(user[0].password == password){
+                res.status(200).json({message: "user login successful",success:true})
+            }else{
+                res.status(401).json({message: "user not authorized",success:false})
+            }
+        }else{
+            res.status(404).json({message:"User not found",success:false})
+        }
+        }
+    catch(err){
+        res.status(500).json({message:err,success:false})
+
+    }
+
+    }
+
+    
+   
+
+
+
+ module.exports = {signup,login};
    
 
 
