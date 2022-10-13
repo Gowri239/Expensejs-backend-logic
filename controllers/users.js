17f70fc1eff4ea53aead9a035caa863dc5c820b7
Gowri239 <gowrimopuru14@gmail.com>
2022-10-13 22:07:44 +0530

    bcrypt

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,11 @@
+const bcrypt = require('bcrypt')
 const User = require('../models/users')
 
 function isStringInvalid(string){
     if(string == undefined || string.length === 0){
         return true
-    } else{
+    } 
+    else{
         return false
     }
 }
@@ -11,13 +13,21 @@ function isStringInvalid(string){
 const signup = async (req,res) => {
     try{
         const {name,email,password} = req.body
+        console.log(email)
         if(isStringInvalid(name) || isStringInvalid(email) || isStringInvalid(password)){
             return res.status(400).json({message: "Enter all details",success: false})
         }
 
-        await User.create({name,email,password})
-            res.status(201).json({message:"successfully created new user" })
-    }catch(err){
+        const saltrounds = 10;
+        const hashPassword = await bcrypt.hash(password,saltrounds)
+        console.log("123",hashPassword)
+    
+        await User.create({name,email,password : hashPassword})
+        res.status(201).json({message:"successfully created new user" })
+        
+    }
+       
+    catch(err){
         res.status(500).json(err)
     }
  }
@@ -26,31 +36,30 @@ const signup = async (req,res) => {
     try{
         const {email,password} = req.body
         if(isStringInvalid(email) || isStringInvalid(password)){
+            console.log(email)
             return res.status(400).json({message: "Enter all details",success:false})
         }
 
         const user = await User.findAll({where: {email}})
         if(user.length>0){
-            if(user[0].password == password){
-                res.status(200).json({message: "user login successful",success:true})
-            }else{
+            bcrypt.compare(password,user[0].password,(req,result) => {
+                if(result === true){
+                    res.status(200).json({message: "user login successful",success:true})
+                }else{
                 res.status(401).json({message: "user not authorized",success:false})
-            }
-        }else{
+                }
+            })
+        }
+        else{
             res.status(404).json({message:"User not found",success:false})
         }
-        }
+    }
     catch(err){
         res.status(500).json({message:err,success:false})
 
     }
 
-    }
-
-    
-   
-
-
+}
 
  module.exports = {signup,login};
    
